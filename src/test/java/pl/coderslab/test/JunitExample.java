a16102f8a2b9c95ba2f55de10b25bf56e13359d9
edu <edu@mail.pl>
2022-08-14 11:02:11 +0200

    junit first example

---
+++ src/test/java/pl/coderslab/test/JunitExample.java
@@ -0,0 +1,35 @@
+package pl.coderslab.test;
+
+import org.junit.jupiter.api.*;
+
+public class JunitExample {
+    @Test
+    public void firstTest() {
+        System.out.println("first test");
+    }
+
+    @Test
+    public void secondTest() {
+        System.out.println("second test");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("this will be run before each test");
+    }
+
+    @AfterEach
+    public void afterEach() {
+        System.out.println("this will be run after each test");
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("this will be run before All tests once");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("this will be run after All tests once");
+    }
+}
