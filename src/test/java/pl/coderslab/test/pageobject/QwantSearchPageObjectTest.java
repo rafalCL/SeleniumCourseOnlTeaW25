7876fbca59fe2338d7f8cb04450418e5b1030189
edu <edu@mail.pl>
2022-08-27 14:59:27 +0200

    random phrase search

--- src/test/java/pl/coderslab/test/pageobject/QwantSearchPageObjectTest.java
+++ src/test/java/pl/coderslab/test/pageobject/QwantSearchPageObjectTest.java
@@ -7,11 +7,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class QwantSearchPageObjectTest {
     private WebDriver driver;
+    private static String[] SEARCH_PHRASES = {
+            "W pustyni i w puszczy",
+            "Quo Vadis",
+            "Szkice piórkiem",
+            "Coders Lab",
+            "Pogoda w Krakowie",
+            "Wakacje"
+    };
 
     @BeforeEach
     public void beforeEach() {
@@ -26,6 +35,29 @@ public class QwantSearchPageObjectTest {
         QwantMainPage qwantMainPage = new QwantMainPage(this.driver);
         // given
         String searchPhrase = "W pustyni i w puszczy";
+        // when
+        qwantMainPage.appendSearchPhrase(searchPhrase);
+        qwantMainPage.clickSearchIcon();
+        // then
+        QwantSearchResultsPage resultsPage = new QwantSearchResultsPage(this.driver);
+        List<String> resultsHeaders = resultsPage.getResultsHeaders();
+        String lowercaseSearchPhrase = searchPhrase.toLowerCase();
+        for (int i = 0; i<4; i++) {
+            String lowerCaseActualPhrase = resultsHeaders.get(i).toLowerCase();
+            if(!lowerCaseActualPhrase.contains(lowercaseSearchPhrase)) {
+                fail(String.format("Element sholud contain %s, but was: %s", lowercaseSearchPhrase, lowerCaseActualPhrase));
+            }
+        }
+        // driver.quit();
+    }
+
+    @Test
+    public void searchRandomPhraseTest() {
+        QwantMainPage qwantMainPage = new QwantMainPage(this.driver);
+        // given
+        Random random = new Random(System.currentTimeMillis());
+        int randomIndex = random.nextInt(SEARCH_PHRASES.length);
+        String searchPhrase = SEARCH_PHRASES[randomIndex];
         // when
         qwantMainPage.appendSearchPhrase(searchPhrase);
         qwantMainPage.clickSearchIcon();
