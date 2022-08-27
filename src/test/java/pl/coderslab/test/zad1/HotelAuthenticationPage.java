d1816fbef121a62c5cb414cb6f4ab8a200592742
edu <edu@mail.pl>
2022-08-27 16:40:40 +0200

    refactor HotelAuthenticationPage to use PageFactory util class to initializa page elements

--- src/test/java/pl/coderslab/test/zad1/HotelAuthenticationPage.java
+++ src/test/java/pl/coderslab/test/zad1/HotelAuthenticationPage.java
@@ -1,21 +1,22 @@
 package pl.coderslab.test.zad1;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HotelAuthenticationPage {
-    private WebDriver driver;
+    @FindBy(css = "input#email_create")
+    private WebElement emailInput;
+    @FindBy(id = "SubmitCreate")
+    private WebElement submitButton;
 
     public HotelAuthenticationPage(WebDriver driver) {
-        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public void submitCreateAnAccountForm(String email) {
-        WebElement emailInput = this.driver.findElement(By.cssSelector("input#email_create"));
         emailInput.sendKeys(email);
-
-        WebElement submitButton = this.driver.findElement(By.cssSelector("button#SubmitCreate"));
         submitButton.click();
     }
 }
